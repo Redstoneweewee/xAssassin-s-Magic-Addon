1b547847da79d40f51c0c0dcc79abaf22c8e0373
Redstoneweewee <95556046+Redstoneweewee@users.noreply.github.com>
2025-04-07 07:53:50 -0700

    Added PlayerObject Framework

---
+++ packs/BP/scripts/Definitions/PlayerDef.js
@@ -0,0 +1,10 @@
+
+import { Player } from '@minecraft/server';
+
+/**
+ * Unused for now, but I'm sure we will need this in the future
+ * as we start giving players properties
+ * @typedef {object} PlayerObjectDef
+ * @property {Player} player
+ * 
+ */
